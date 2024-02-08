a5ea75ba9f487c546e25a14c704424b22e519452
Orlando Caraballo <orlandocaraballo@gmail.com>
2024-02-08 13:29:40 -0500

    Created initial fight page

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -7,19 +7,20 @@ interface CharacterProps {
     image: string;
     abilities: string[];
   };
+  playerOne?: boolean;
 }
 
-const Character = ({ character }: CharacterProps) => {
+const Character = ({ character, playerOne }: CharacterProps) => {
   const { name, hp, attack, defense, image, abilities } = character;
 
   return (
-    <div className="p-4 rounded shadow-lg bg-slate-200">
+    <div className="p-4 rounded shadow-lg bg-slate-200 w-80">
       <p className="flex justify-between">
         <span className="text-purple-700 text-2xl">{hp}</span>
         <span className="text-blue-700 text-2xl">{attack} </span>
         <span className="text-red-700 text-2xl">{defense}</span>
       </p>
-      <img src={image} />
+      <img className={playerOne ? "-scale-x-100" : ""} src={image} />
       <section className="flex justify-between border-l-0 border-r-0 border-b-0 border border-t-slate-500 border-solid pt-4">
         <h2 className="text-green-700 capitalize text-2xl">{name}</h2>
 
--- src/components/Fight.tsx
+++ src/components/Fight.tsx
@@ -1,7 +1,39 @@
+import Character from "./Character";
+
 const Battler = () => {
   return (
     <>
       <h1 className="text-6xl font-bold mb-4 mt-8">Fight!</h1>
+      <div className="flex justify-between">
+        <section>
+          <Character
+            character={{
+              name: "Character 1",
+              hp: 20,
+              attack: 5,
+              defense: 22,
+              abilities: ["throw poop", "eat", "shower"],
+              image:
+                "https://www.fightersgeneration.com/np2/char2/char/guile-sfa3-side.jpg",
+            }}
+            playerOne={true}
+          />
+        </section>
+        <section className="self-center font-bold text-8xl">VS</section>
+        <section>
+          <Character
+            character={{
+              name: "Character 2",
+              hp: 10,
+              attack: 50,
+              defense: 56,
+              abilities: ["throw poop", "eat", "shower"],
+              image:
+                "https://www.fightersgeneration.com/np2/char2/char/guile-sfa3-side.jpg",
+            }}
+          />
+        </section>
+      </div>
     </>
   );
 };
