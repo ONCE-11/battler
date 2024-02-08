868458db5a6c1fe79856c033e8c1a3a013aa1119
Orlando Caraballo <orlandocaraballo@gmail.com>
2024-02-08 18:22:04 -0500

    Made ability button behave more like a button

--- src/components/Fight.tsx
+++ src/components/Fight.tsx
@@ -65,7 +65,7 @@ const Battler = () => {
         <section className="mt-6">
           {playerTwo.abilities.map((ability) => (
             <button
-              className="first:ml-0 ml-10 bg-slate-500 py-2 px-4 text-white rounded"
+              className="first:ml-0 ml-10 bg-slate-500 py-2 px-4 text-white rounded active:shadow-inner active:bg-slate-600"
               onClick={(e) => handleClick(e, ability)}
             >
               {ability.name}
