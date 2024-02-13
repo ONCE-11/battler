6b98c8cbb8c064cb47a36a2f3b60ba4f23d47849
Orlando Caraballo <orlandocaraballo@gmail.com>
2024-02-13 15:14:30 -0500

    Added supabase functions Fixed a minor warning

--- src/components/Fights.tsx
+++ src/components/Fights.tsx
@@ -1,4 +1,27 @@
+// import { supabase } from "./utilities";
+
 const Fights = () => {
+  // const changes = supabase
+  //   .channel("table-filter-changes")
+  //   .on(
+  //     "postgres_changes",
+  //     {
+  //       event: "UPDATE",
+  //       schema: "public",
+  //       table: "profiles",
+  //       filter: "id=eq.ede554ef-f3c1-4437-b509-866b3f20dafc",
+  //     },
+  //     (payload) => console.log(payload)
+  //   )
+  //   .subscribe();
+  // const handleClick = async () => {
+  //   console.log("click");
+  //   const response = await supabase.functions.invoke("hello", {
+  //     body: { name: "Functions" },
+  //   });
+  //   console.log(response);
+  // };
+  // return <button onClick={handleClick}>Test Function</button>;
   return "Fights";
 };
 
