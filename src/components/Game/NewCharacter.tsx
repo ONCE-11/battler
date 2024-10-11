45b26a27cc316afef62bc933488f98404d81c6c1
Orlando Caraballo <orlando@once.services>
2024-10-10 20:28:52 -0400

    Ensures correct error handling is used for createCharacter edge function Fixes #19

--- src/components/Game/NewCharacter.tsx
+++ src/components/Game/NewCharacter.tsx
@@ -3,7 +3,13 @@ import { supabase } from "../../utils";
 import { characterAtom, currentUserAtom } from "../../atoms";
 import { useAtomValue, useSetAtom } from "jotai";
 import Button from "../Button";
-import { User } from "@supabase/supabase-js";
+import {
+  FunctionsFetchError,
+  FunctionsHttpError,
+  FunctionsRelayError,
+  User,
+} from "@supabase/supabase-js";
+import { CharacterWithAbilities } from "../../types/custom";
 
 export default function NewCharacter() {
   const currentUser = useAtomValue(currentUserAtom);
@@ -15,15 +21,28 @@ export default function NewCharacter() {
   }
 
   const handleClick = async (currentUser: User) => {
-    const { data: character, error } = await supabase.functions.invoke(
-      "createCharacter",
-      {
-        body: { userId: currentUser.id },
-      }
-    );
+    const { data: character, error } =
+      await supabase.functions.invoke<CharacterWithAbilities>(
+        "createCharacter",
+        {
+          body: { userId: currentUser.id },
+        }
+      );
 
-    if (error) {
-      console.error(error);
+    if (error instanceof FunctionsHttpError) {
+      const errorMessage = await error.context.json();
+      console.error("Function returned an error", errorMessage);
+      return;
+    } else if (error instanceof FunctionsRelayError) {
+      console.error("Relay error:", error.message);
+      return;
+    } else if (error instanceof FunctionsFetchError) {
+      console.error("Fetch error:", error.message);
+      return;
+    }
+
+    if (!character) {
+      console.error("Character is not set");
       return;
     }
 
