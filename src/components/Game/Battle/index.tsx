e5035e486ac5e47e9ea19e223562286ed7653fe8
Orlando Caraballo <orlando@once.services>
2024-11-05 20:38:20 -0500

    Fixes #15

--- src/components/Game/Battle/index.tsx
+++ src/components/Game/Battle/index.tsx
@@ -1,12 +1,14 @@
-import { atom, useAtom, useSetAtom } from "jotai";
+import { atom, useSetAtom } from "jotai";
 import Title from "../../Title";
 import { useEffect, useMemo, useState } from "react";
 import { supabase } from "../../../utils";
-import { CharacterWithAbilities } from "../../../types/custom";
+import { CharacterWithAbilities, Scene } from "../../../types/custom";
 import Player from "./Player";
 import { Tables } from "../../../types/supabase";
 import { FightWithPlayers } from "../types.js";
 import { RealtimePostgresInsertPayload } from "@supabase/supabase-js";
+import Button from "../../Button.js";
+import { sceneAtom } from "../../../atoms.js";
 
 type BattleProps = {
   fight: FightWithPlayers;
@@ -24,27 +26,19 @@ type RealtimePayloadData = {
   };
 };
 
-const player1DisabledAtom = atom(false);
-const player2DisabledAtom = atom(false);
-const gameOverTitleAtom = atom("");
-
 export default function Battle({ fight, character }: BattleProps) {
   const [player1, setPlayer1] = useState(fight.player1);
   const [player2, setPlayer2] = useState(fight.player2);
 
   const setTurn = useSetAtom(useMemo(() => atom(fight.turn), [fight.turn]));
-  const [gameOver, setGameOver] = useAtom(
-    useMemo(() => atom(fight.game_over), [fight.game_over])
+  const [gameOver, setGameOver] = useState(fight.game_over);
+  const [player1Disabled, setPlayer1Disabled] = useState(false);
+  const [player2Disabled, setPlayer2Disabled] = useState(false);
+  const [currentTurnPlayerId, setCurrentTurnPlayerId] = useState(
+    fight.current_turn_player_id
   );
-  const [player1Disabled, setPlayer1Disabled] = useAtom(player1DisabledAtom);
-  const [player2Disabled, setPlayer2Disabled] = useAtom(player2DisabledAtom);
-  const [currentTurnPlayerId, setCurrentTurnPlayerId] = useAtom(
-    useMemo(
-      () => atom(fight.current_turn_player_id),
-      [fight.current_turn_player_id]
-    )
-  );
-  const [gameOverTitle, setGameOverTitle] = useAtom(gameOverTitleAtom);
+  const [winnerId, setWinnerId] = useState(fight.winner_id);
+  const setScene = useSetAtom(sceneAtom);
 
   useEffect(function () {
     if (character.id === fight.player1_id) {
@@ -80,11 +74,8 @@ export default function Battle({ fight, character }: BattleProps) {
     setTimeout(() => {
       setTurn(turn);
       setCurrentTurnPlayerId(current_turn_player_id);
+      setWinnerId(winner_id);
       setGameOver(game_over);
-
-      if (game_over) {
-        setGameOverTitle(winner_id === player1.id ? "P1 Wins" : "P2 Wins");
-      }
     }, 1000);
   }
 
@@ -131,7 +122,31 @@ export default function Battle({ fight, character }: BattleProps) {
   return (
     <>
       {gameOver ? (
-        <Title>{gameOverTitle}</Title>
+        <>
+          <Title className="flex justify-between">
+            {winnerId === character.id ? (
+              <>
+                <span>You win!</span>
+                <Button
+                  handleClick={() => setScene(Scene.Beef)}
+                  className={`text-xl`}
+                >
+                  Take your W
+                </Button>
+              </>
+            ) : (
+              <>
+                <span>You lost!</span>
+                <Button
+                  handleClick={() => setScene(Scene.NewCharacter)}
+                  className={`text-xl`}
+                >
+                  Dip out
+                </Button>
+              </>
+            )}
+          </Title>
+        </>
       ) : (
         <Title>
           {currentTurnPlayerId === player1.id ? "P1's Turn" : "P2's Turn"}
