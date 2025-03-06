1f3d896cb6575ecec713047b60eddd8cd3acde0e
Orlando Caraballo <orlando@once.services>
2025-03-06 18:12:05 -0500

    Added check for dev environment before adding supabase headers.

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,26 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./types/supabase";
 
-const { VITE_SUPABASE_URL, VITE_SUPABASE_KEY } = import.meta.env;
+const { VITE_SUPABASE_URL, VITE_SUPABASE_KEY, VITE_ENV } = import.meta.env;
+
+let globalSupabaseHeaders;
 
 export const ngrokTunnelHeaders = {
   "ngrok-skip-browser-warning": "true",
 };
+
+if (VITE_ENV === "development") {
+  globalSupabaseHeaders = { ...ngrokTunnelHeaders };
+} else {
+  globalSupabaseHeaders = {};
+}
 
 export const supabase = createClient<Database>(
   VITE_SUPABASE_URL,
   VITE_SUPABASE_KEY,
   {
     global: {
-      headers: { ...ngrokTunnelHeaders },
+      headers: globalSupabaseHeaders,
     },
   }
 );
